90a13118355d60d99c590be26be74e2bcab32325
Adri Antori <adriantori11@gmail.com>
2023-09-01 20:32:42 +0700

    fix: changed constants to deployed BE

--- FE/src/constants.ts
+++ FE/src/constants.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://adriantori-w11-be.up.railway.app';
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'https://adriantori-w11-be.up.railway.app';
+//export const BASE_URL = 'http://localhost:8080';
